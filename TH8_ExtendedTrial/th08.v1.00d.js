6fc1107184576288fbf64e7571d63a19bfbc2915
zero318 <zero318@users.noreply.github.com>
2020-10-20 00:08:58 -0400

    Forgot to update version number

--- TH8_ExtendedTrial/th08.v1.00d.js
+++ TH8_ExtendedTrial/th08.v1.00d.js
@@ -1,5 +1,5 @@
 {
-	"COMMENT": "Touhou 8 Imperishable Night: Extended Trial v0.01 - I honestly have no idea if the spaces are necessary between the digits, but it's working and I'm not questioning it.",
+	"COMMENT": "Touhou 8 Imperishable Night: Extended Trial v0.02 - I honestly have no idea if the spaces are necessary between the digits, but it's working and I'm not questioning it.",
 	"binhacks": {
 		"TH8ExtendedTrial_TimesRanksTable": {
 			"addr": "RxC77F0",
