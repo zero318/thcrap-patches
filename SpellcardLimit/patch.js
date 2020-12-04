fff0c9409303d886a2b5bdf939fcbd5d81c80df7
zero318 <zero318@users.noreply.github.com>
2020-12-03 21:41:31 -0500

    Create patch.js

---
+++ SpellcardLimit/patch.js
@@ -0,0 +1,7 @@
+{
+    "id": "SpellcardLimit",
+    "servers": [
+        "https://mirrors.thpatch.net/zero318/SpellcardLimit/"
+    ],
+    "title": "Increases the maximum number of available spellcard IDs."
+}
