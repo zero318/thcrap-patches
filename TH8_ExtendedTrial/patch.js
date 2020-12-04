79e9d3d7b8da43039d6759bc2afcdde38009c1b1
zero318 <zero318@users.noreply.github.com>
2020-12-03 21:42:20 -0500

    Update patch.js

--- TH8_ExtendedTrial/patch.js
+++ TH8_ExtendedTrial/patch.js
@@ -1,7 +1,10 @@
 {
 	"id": "TH8_ExtendedTrial",
-	"title": "Attempts to port as many features of the old IN trials as possible to the final version.",
 	"dependencies": [
 		"nmlgc/base_tsa"
-	]
+	],
+	"servers": [
+        	"https://mirrors.thpatch.net/zero318/TH8_ExtendedTrial/"
+    	],
+	"title": "Attempts to port as many features of the old IN trials as possible to the final version.",
 }
